ac89840c314baf54d3e4e59bd79cc6a4130672ec
jellygas <vologi1@naver.com>
2024-12-02 14:57:56 +0900

    Update 뭘꼬라보냐.js

--- 뭘꼬라보냐.js
+++ 뭘꼬라보냐.js
@@ -1,12 +1,8 @@
-<script>
-    let selectBox = document.getElementById("selectBox");
-    let resultDiv = document.getElementById("result");
-    
-    selectBox.addEventListener("change", function() {
-        let select = selectBox.options[selectBox.selectedIndex].value;
-        resultDiv.innerText = "좋아하는 음류수는: " + select;
-    });
-    </script>
-    </body>
-    </html>
+let selectBox = document.getElementById("selectBox");
+let resultDiv = document.getElementById("result");
+selectBox.addEventListener("change", function() {
+    let select = selectBox.options[selectBox.selectedIndex].value;
+    resultDiv.innerText = "좋아하는 차는: " + select;
+});
+
     
