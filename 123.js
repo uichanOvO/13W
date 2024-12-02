632301b1b639508966ec4650105aecd1fdda39a3
uichanOvO <dmlcks74100@gmail.com>
2024-12-02 15:09:51 +0900

    Update 123.js

--- 123.js
+++ 123.js
@@ -3,5 +3,5 @@ let resultDiv = document.getElementById("result");
 
 BOX.addEventListener("change", function() {
     let select = BOX.options[BOX.selectedIndex].value;
-    resultDiv.innerText = "마시고 싶은 카페 음료는?:" + select;
+    resultDiv.innerText = "하고 싶은 게임은?:" + select;
 });
